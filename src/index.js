b311cec3cdfa7fa2ac4ecdaffdb26c487a390cee
JoelTorres981 <joel.torres@epn.edu.ec>
2026-02-03 19:13:03 -0500

    Sin chat en tiempo real

--- src/index.js
+++ src/index.js
@@ -2,33 +2,9 @@
 
 import app from './server.js';
 import connection from './database.js';
-import http from 'http';
-import { Server } from 'socket.io';
 
 connection();
 
-const server = http.createServer(app);
-
-const io = new Server(server, {
-    cors: {
-        origin: process.env.FRONTEND_URL || "*", // Permite configurar el origen en producción o usar * por defecto
-        methods: ["GET", "POST"]
-    }
-});
-
-io.on('connection', (socket) => {
-    console.log('Usuario conectado al chat:', socket.id);
-
-    socket.on('chat_message', (data) => {
-        // data esperada: { user: "Nombre", message: "Hola", time: "10:00 AM" }
-        io.emit('chat_message', data);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('Usuario desconectado:', socket.id);
-    });
-});
-
-server.listen(app.get('port'), () => {
-    console.log(`Server & Socket.io running on http://localhost:${app.get('port')}`);
+app.listen(app.get('port'), () => {
+    console.log(`Server ok on http://localhost:${app.get('port')}`);
 });
